ec0ec6a2df977b1be7b0d2c4bc3fae2f789b823c
HACKERX6661 <daniilprobanitvseh@gmail.com>
2026-02-06 20:24:15 +0100

    Refactor news fetching and update HTML content

--- update_news.py
+++ update_news.py
@@ -1,37 +1,31 @@
 import feedparser
 
-# Liste der Quellen (RSS-Feeds)
-FEEDS = ["https://www.tagesschau.de/infosilla/headlines/index~rss2.xml"]
+FEEDS = [
+    "https://www.tagesschau.de/infosilla/headlines/index~rss2.xml",
+    "https://rss.orf.at/news.xml"
+]
 
-def fetch_news():
-    html_output = ""
+def get_news():
+    html = ""
     for url in FEEDS:
         feed = feedparser.parse(url)
-        # Nimmt die aktuellsten 15 Meldungen
-        for entry in feed.entries[:15]:
-            description = entry.description if 'description' in entry else ""
-            published = entry.published if 'published' in entry else "Unbekannt"
-            
-            html_output += f'''
+        for entry in feed.entries[:5]:
+            html += f'''
             <div class="news-item">
+                <div class="date">{entry.get('published', 'Aktuell')}</div>
                 <h3><a href="{entry.link}" target="_blank">{entry.title}</a></h3>
-                <p>{description}</p>
-                <div class="date">Stand: {published}</div>
-            </div>
-            '''
-    return html_output
+                <p>{entry.get('description', '')[:300]}...</p>
+            </div>'''
+    return html
 
-# HTML-Datei lesen
 with open("index.html", "r", encoding="utf-8") as f:
     content = f.read()
 
-# News-Bereich im HTML finden und ersetzen
-start_marker = '<div id="news-container">'
-end_marker = '</div>'
-parts = content.split(start_marker)
-top_part = parts[0] + start_marker
-bottom_part = end_marker + parts[1].split(end_marker)[-1]
+# Sucht die Markierungen und tauscht den Inhalt dazwischen aus
+start_mark = ""
+end_mark = ""
 
-# Datei mit neuen Inhalten überschreiben
+new_content = content.split(start_mark)[0] + start_mark + get_news() + end_mark + content.split(end_mark)[1]
+
 with open("index.html", "w", encoding="utf-8") as f:
-    f.write(top_part + fetch_news() + bottom_part)
+    f.write(new_content)
